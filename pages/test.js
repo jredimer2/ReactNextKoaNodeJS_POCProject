478639bd661befd56b6db24eafe6c24d67025344
Raj Kumar <rajkmr.rkp@gmail.com>
2020-11-30 14:28:05 +0530

    multiple queries to a page

--- pages/test.js
+++ pages/test.js
@@ -5,13 +5,16 @@ import Users from '../components/Users'
 
 function Test(props) {
 
+  // step4: destructure merchants and users and pass them to Merchants and Users Components
+  const { merchants, users } = props;
+
   return (
     <Page title="Multiple queries to a page">
       <Card>
-        <Merchants jsonResponse={props} />
+        <Merchants jsonResponse={merchants} />
       </Card>
       <Card>
-        <Users jsonResponse={props} />
+        <Users jsonResponse={users} />
       </Card>
     </Page>
 
@@ -20,9 +23,16 @@ function Test(props) {
 
 // This is to query Merchants list
 Test.getInitialProps = async function () {
-  const res = await fetch(`http://localhost:3001/merchants`)
-  const data = await res.json()
-  return { data }
+  // step1: get merchants
+  const res1 = await fetch(`http://localhost:3001/merchants`)
+  const merchants = await res1.json()
+
+  // step2: get users
+  const res2 = await fetch(`http://localhost:3001/users`)
+  const users = await res2.json()
+
+  // step3: return them both together and in Test function destructure them
+  return { merchants, users }
 }
 
 // Question: How do I query Users list? there's only one getInitialProps()
